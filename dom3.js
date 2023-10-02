b35eef4882c186d5c070871c2ae91a78ddd9f122
Abhashkumar64 <abhashkumar64>
2023-10-03 03:00:45 +0530

    dom list

---
+++ dom3.js
@@ -0,0 +1,6 @@
+var items = document.getElementsByClassName('list-group-item-new');
+
+items[0].style.backgroundColor='green';
+
+var item5=document.getElementsByTagName('li');
+item5[4].style.color = 'red';
