197d150649f2ac30209cbf4e6cfeb12907d69830
Abhashkumar64 <abhashkumar64>
2023-10-03 03:31:08 +0530

    dom query selector

---
+++ dom4.js
@@ -0,0 +1,4 @@
+var items = document.querySelector('.list-group-item:nth-child(2)');
+items.style.backgroundColor='green';
+var items3 = document.querySelector('.list-group-item:nth-child(3)');
+items3.style.color='white';
