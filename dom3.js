3bf133a54ad702ba2b28c7fd1c146eccc58dec25
Abhashkumar64 <abhashkumar64>
2023-10-08 19:57:44 +0530

    promise

--- dom3.js
+++ dom3.js
@@ -1,6 +1,30 @@
-var items = document.getElementsByClassName('list-group-item-new');
+var form = document.getElementById('addForm');
+var itemList = document.getElementById('items');
 
-items[0].style.backgroundColor='green';
 
-var item5=document.getElementsByTagName('li');
-item5[4].style.color = 'red';
+form.addEventListener('submit', addItem);
+itemList.addEventListener('click', removeItem);
+
+function addItem(e){
+  e.preventDefault();
+  var newItem = document.getElementById('item').value;
+  var li = document.createElement('li');
+  li.className = 'list-group-item';
+  li.appendChild(document.createTextNode(newItem));
+
+  var deleteBtn = document.createElement('button');
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  deleteBtn.appendChild(document.createTextNode('X'));
+  li.appendChild(deleteBtn);
+  itemList.appendChild(li);
+}
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
+    }
+  }
+}
+
+
